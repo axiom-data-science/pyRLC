6c33f148feaa13548a549610d0087bb2130c7fde
Jesse Lopez <jesse@axiomdatascience.com>
2017-08-30 18:16:05 -0700

    add more bits.  test failing for full record scan.

--- pyRLC.py
+++ pyRLC.py
@@ -1,6 +1,13 @@
 #!python
 # utf-8
-"""Methods for reading .rlc files and converting to .png and .geotiff."""
+"""Methods for reading .rlc files and converting to .png and .geotiff.
+
+rlc File format (type 4):
+- Header - 8 bytes
+- Sweeps
+    - Header - 7 bytes
+    - Scan data - dependent on file
+"""
 import os
 
 import numpy as np
@@ -8,16 +15,176 @@ import numpy as np
 
 class RLCfile:
     """All methods for manipulating .rcl files."""
+    NMEA_SIZE = 240
+
     def __init__(self, fpath):
-        file_path = os.path.abspath(fpath) 
+        self.file_path = os.path.abspath(fpath) 
+        self.file_size = os.path.getsize(self.file_path) 
 
 #        with open(file_path, 'rb') as f:
 #            (filesig, rlctype, _) = read_header(f)
 
     def read_header(self, f):
-        """Reads and returns header."""
+        """Read and return header data"""
         filesig = np.frombuffer(f.read(4), np.uint8)
         rlctype = np.frombuffer(f.read(1), np.uint8)
         three_zeros = np.frombuffer(f.read(3), np.uint8)
 
         return (filesig, rlctype, three_zeros)
+
+
+    def read_record_data_type4(self, f):
+        """Read and return record data (RLC type 4)"""
+        sweep_header = self.read_sweep_header(f)
+        # scandata - X dim - NscanX
+        self.samples_per_scan = sweep_header['scanlines_per_sweep_ext']
+        # scandata - Y dim - Nsamp
+        self.samples_per_sweep = sweep_header['samples_per_scanline']
+        self.scandata = np.zeros((self.samples_per_scan,
+                                  self.samples_per_sweep))
+        # scanline numbers used for reprojection - scan_i
+        self.scanline_nums = np.zeros((self.samples_per_scan,))
+
+        scan = 0
+        fpos1 = f.tell()
+        while fpos1 <= self.file_size:
+            scan_header = self.read_scan_header(f)
+
+            if scan_header['utype'] == 0:
+                seg_info = self.read_extended_segment_info(f)
+
+                if (seg_info['number'] >= 0) & (seg_info['number'] <= self.samples_per_sweep):
+                    if scan_header['compressed']:
+                        self.scandata[scan, :] = self.read_compressed_scanline(f)
+                    else:
+                        fpos0 = f.tell()
+                        self.scandata[scan, :] = np.frombuffer(f.read(self.samples_per_sweep), np.uint8)
+                        fpos1 = f.tell()
+                    self.scanline_nums[scan] = seg_info['number']
+                    scan += 1
+                else:
+                    # end of file
+                    fpos1 = self.file_size + 1
+            elif scan_header['utype'] > 0:
+                # read NMEA header 
+                self.NMEAhead = self.read_NMEA_header(f)
+            else:
+                # At end of file
+                fpos1 = self.file_size + 1
+
+        Nscan = scan - 1
+        scandata = self.scandata[:Nscan, :]
+        scan_i = self.scanline_nums[:Nscan]
+
+        return (scandata, Nscan, scan_i) 
+
+    def read_sweep_header(self, f):
+        """Read and return individual sweep header"""
+        sweep_header = {}
+        sweep_header['sample_rate'] = read_uint32(f)
+        sweep_header['samples_per_scanline'] = read_uint32(f) 
+        sweep_header['scanlines_per_sweep'] = read_uint32(f) 
+        sweep_header['scanlines_per_sweep_ext'] = read_uint32(f) 
+        sweep_header['time_of_sweep_pc'] = read_uint32(f) 
+        sweep_header['time_of_sweep_vp'] = read_uint64(f) 
+        sweep_header['uspare'] = read_uint32(f) 
+
+        return sweep_header
+
+    def read_scan_header(self, f):
+        """Read header for individual scan
+        
+        Notes:
+        - bits 1-3 (sum) : utype (0: scan data; else: NMEA data)
+        - bits 4-7 (sep) : urange
+        - bit 8          : compressed (specifies if following is runlength encoded)
+        
+        """
+        scanhead = read_uint8(f) 
+
+        if scanhead > 0:
+            rsi = np.zeros((8,))
+            b = 0
+            while scanhead > 0:
+                rsi[b] = scanhead % 2
+                scanhead = scanhead // 2
+                b += 1
+
+            utype = rsi[:3].sum()
+            urange = rsi[3:7]
+            compressed = rsi[-1]
+        else:
+            utype = -1
+            urange = -1
+            compressed = -1
+        
+        return {'utype': utype, 'urange': urange, 'compressed': compressed}
+
+    def read_NMEA_header(self, f):
+        """Read and return NMEA header"""
+        return np.frombuffer(f.read(self.NMEA_SIZE), np.uint8, count=240)
+
+    def read_extended_segment_info(self, f):
+        """Read extended segment info"""
+        number = read_uint32(f) 
+        time = read_uint32(f) 
+
+        if type(number) is not np.uint32:
+            number = -1
+            time = np.nan
+
+        return {'number': number, 'time': time}
+
+    def read_scanline(self, f):
+        """Read scanline"""
+        if self.scanhead['compressed'] == 0:
+            self.scandata[scan, :] = to_int(f.read(Nsamp)) 
+#            self.scanlines[scan] = 1
+        else:
+            fpos0 = f.tell()
+            self.scandata[scan, :] = self.decompress_scanline(f)
+#            self.scanlines[scan] = 1
+            fpos1 = f.tell()
+
+    def _read_compressed_scanline(self, f):
+        """Read run-length encoded scanline 
+        
+        Notes:
+        - Corresponds to Xenex/RTI's "RLC_New"
+        """
+        self.fpos1 = f.tell()
+        self.scandata[scan, :] = self.decompress_scanline(f)
+        self.scanlines[scan] = 1
+        self.fpos2 = f.tell()
+
+    def read_compressed_scanline(self, f):
+        """Decompress run-length compressed scanline"""
+        Nsamp = self.samples_per_sweep
+        scanline = np.zeros((self.samples_per_sweep,))
+        fpos0 = f.tell()
+        samprem = self.file_size - fpos0
+        samp = 0 
+
+        while (samp < Nsamp) & (samp <= samprem):
+            backscat = read_uint8(f)
+            if backscat == 255:
+                repval = read_uint8(f) + 1
+                backscat = read_uint8(f)
+
+                scanline[samp:samp+repval] = backscat
+                samp += repval
+            else:
+                scanline[samp] = backscat
+                samp += 1
+
+        return scanline
+
+
+def read_uint8(f):
+    return np.frombuffer(f.read(1), np.uint8)[0]
+
+def read_uint32(f):
+    return np.frombuffer(f.read(4), np.uint32)[0]
+
+def read_uint64(f):
+    return np.frombuffer(f.read(8), np.uint64)[0]
--- tests/test_read.py
+++ tests/test_read.py
@@ -1,7 +1,9 @@
 import pyRLC
+import numpy as np
 
 
 TEST_FILE = 'tests/test.rec'
+
 
 def test_read_header():
     rlc_file = pyRLC.RLCfile(TEST_FILE)
@@ -22,3 +24,123 @@ def test_read_header():
         assert sparebytes[0] == 0
         assert sparebytes[1] == 0
         assert sparebytes[2] == 0
+
+
+def test_read_sweep_header():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # skip header 
+        f.seek(8)
+
+        sweep_header = rlc_file.read_sweep_header(f)
+        assert sweep_header['sample_rate'] == 54000000 
+        assert sweep_header['samples_per_scanline'] == 512 
+        assert sweep_header['scanlines_per_sweep'] == 1024
+        assert sweep_header['scanlines_per_sweep_ext'] == 4096 
+        assert sweep_header['time_of_sweep_pc'] == 1973129824 
+        assert sweep_header['time_of_sweep_vp'] == 18313917820654764054 
+        assert sweep_header['uspare'] == 0
+
+
+def test_read_scan_header():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # skip header and sweep header
+        f.seek(40)
+
+        scan_header = rlc_file.read_scan_header(f)
+        assert scan_header['utype'] == 1
+        assert np.array_equal(scan_header['urange'], np.array([0, 0, 0, 0]))
+        assert scan_header['compressed'] == 0 
+
+
+def test_read_NMEA_header():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # skip header, sweep header, and scan head
+        f.seek(41)
+
+        NMEA_header = rlc_file.read_NMEA_header(f)
+        assert NMEA_header[0] == 35
+        assert NMEA_header[1] == 0
+        assert NMEA_header[8] == 180
+        assert NMEA_header[9] == 14
+        assert NMEA_header[-1] == 0
+        assert NMEA_header.sum() == 906
+
+
+def test_read_scanhead2():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # skip header, sweep header, scan head, NMEA header
+        f.seek(281)
+
+        scanhead = rlc_file.read_scan_header(f)
+        assert scanhead['utype'] == 0
+        assert np.array_equal(scanhead['urange'], np.array([1, 0, 1, 0]))
+        assert scanhead['compressed'] == 1 
+
+
+def test_read_extended_segment_info():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # skip
+        f.seek(282)
+
+        extended_info = rlc_file.read_extended_segment_info(f)
+        assert extended_info['number'] == 0
+        assert extended_info['time'] == 4264016289 
+
+
+def test_read_compressed_scanline():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # Read header and save required info
+        # TODO: Pull this out of that method 
+        f.seek(8) 
+        rlc_file.read_record_data_type4(f)
+
+        # skip to my lou
+        f.seek(290)
+
+        scandata = rlc_file.read_compressed_scanline(f)
+        assert scandata[0] == 5
+        assert scandata[-1] == 0
+        assert scandata.sum() == 6522
+
+
+def test_read_compressed_scanline2():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # Read header and save required info
+        f.seek(8) 
+        rlc_file.read_record_data_type4(f)
+
+        # skip to my lou
+        f.seek(799)
+
+        scandata = rlc_file.read_compressed_scanline(f)
+        assert scandata[0] == 168 
+        assert scandata[-1] == 10 
+        assert scandata.sum() == 7213 
+
+
+def test_read_rec_data():
+    rlc_file = pyRLC.RLCfile(TEST_FILE)
+
+    with open(TEST_FILE, 'rb') as f:
+        # Read header and save required info
+        f.seek(8) 
+        (scandata, Nscan, scan_i) = rlc_file.read_record_data_type4(f)
+        import pdb; pdb.set_trace()
+        assert Nscan == 2453
+        assert scan_i.sum() == 5045703
+        assert scandata.sum().sum() == 16223096
+
